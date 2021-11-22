6a0264ea43240436adb13907952c857132fd45cc
Zbynek Konecny <zbynek1729@gmail.com>
2021-11-22 14:23:51 +0100

    Update imports

--- web/src/main/java/org/geogebra/web/full/gui/toolbarpanel/ContextMenuTV.java
+++ web/src/main/java/org/geogebra/web/full/gui/toolbarpanel/ContextMenuTV.java
@@ -23,7 +23,7 @@ import org.geogebra.web.html5.gui.util.AriaMenuItem;
 import org.geogebra.web.html5.main.AppW;
 import org.geogebra.web.html5.util.TestHarness;
 import org.geogebra.web.resources.SVGResource;
-import org.geogebra.web.shared.components.DialogData;
+import org.geogebra.web.shared.components.dialog.DialogData;
 
 import com.google.gwt.user.client.Command;
 
