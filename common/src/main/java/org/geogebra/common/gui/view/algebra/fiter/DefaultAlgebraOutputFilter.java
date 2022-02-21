39fb20891a2180c708677bf8dc11ab54f8946edc
Bence Schneider <bence.schneider@geogebra.at>
2022-02-21 13:22:16 +0100

    APPS-3639 If command with more arguments enabled in CAS

--- common/src/main/java/org/geogebra/common/gui/view/algebra/fiter/DefaultAlgebraOutputFilter.java
+++ common/src/main/java/org/geogebra/common/gui/view/algebra/fiter/DefaultAlgebraOutputFilter.java
@@ -1,5 +1,9 @@
 package org.geogebra.common.gui.view.algebra.fiter;
 
+import org.geogebra.common.kernel.arithmetic.Command;
+import org.geogebra.common.kernel.commands.Commands;
+import org.geogebra.common.kernel.geos.GeoFunction;
+import org.geogebra.common.kernel.geos.GeoSymbolic;
 import org.geogebra.common.kernel.kernelND.GeoElementND;
 
 /**
@@ -9,6 +13,11 @@ public class DefaultAlgebraOutputFilter implements AlgebraOutputFilter {
 
 	@Override
 	public boolean isAllowed(GeoElementND element) {
-		return true;
+		boolean allowed = true;
+		if (element instanceof GeoSymbolic
+				&& Commands.If.name().equals(((Command) element.getDefinition().getLeft()).getName())) {
+			allowed = false;
+		}
+		return allowed;
 	}
 }
