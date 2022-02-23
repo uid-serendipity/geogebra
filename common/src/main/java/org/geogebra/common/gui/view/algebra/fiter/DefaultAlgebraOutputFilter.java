3f6b4613c46fbd00f014c2ac480c1c84d0107878
Balazs Bencze <balazs.bencze@geogebra.org>
2022-02-23 15:27:04 +0200

    APPS-3639 Fix converting Command to string similar to expression node and MyList

--- common/src/main/java/org/geogebra/common/gui/view/algebra/fiter/DefaultAlgebraOutputFilter.java
+++ common/src/main/java/org/geogebra/common/gui/view/algebra/fiter/DefaultAlgebraOutputFilter.java
@@ -1,8 +1,5 @@
 package org.geogebra.common.gui.view.algebra.fiter;
 
-import org.geogebra.common.kernel.arithmetic.Command;
-import org.geogebra.common.kernel.commands.Commands;
-import org.geogebra.common.kernel.geos.GeoSymbolic;
 import org.geogebra.common.kernel.kernelND.GeoElementND;
 
 /**
@@ -12,13 +9,6 @@ public class DefaultAlgebraOutputFilter implements AlgebraOutputFilter {
 
 	@Override
 	public boolean isAllowed(GeoElementND element) {
-		boolean allowed = true;
-		if (element instanceof GeoSymbolic
-				&& element.getDefinition().getLeft() instanceof Command
-				&& Commands.If.name()
-				.equals(((Command) element.getDefinition().getLeft()).getName())) {
-			allowed = false;
-		}
-		return allowed;
+		return true;
 	}
 }
