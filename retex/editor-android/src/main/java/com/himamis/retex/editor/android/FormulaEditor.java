2b5c060c86c6ee8887ffd78652ca51fab3247975
Zbynek Konecny <zbynek@geogebra.at>
2022-02-23 18:44:21 +0100

    APPS-3498 Fix Android compile problem

--- retex/editor-android/src/main/java/com/himamis/retex/editor/android/FormulaEditor.java
+++ retex/editor-android/src/main/java/com/himamis/retex/editor/android/FormulaEditor.java
@@ -571,8 +571,4 @@ public class FormulaEditor extends View implements MathField {
         mMathFieldInternal.parse(text);
     }
 
-    @Override
-    public void setPlainText(String text) {
-        mMathFieldInternal.setPlainText(text);
-    }
 }
